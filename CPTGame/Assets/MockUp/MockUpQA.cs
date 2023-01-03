a377043086f700cd5478318965fb8256beb7e40d
zabryant89 <zachary.a.bryant1@gmail.com>
2023-01-03 14:51:05 -0500

    Desktop Check and Begin Mock Up
    Checking to ensure I could push/pull from my desktop because I initialized this all on my laptop.  Also the early beginnings of the mock up are here (3 simple questions).  I will work on this over the next few days to have ready to present to the team.

---
+++ CPTGame/Assets/MockUp/MockUpQA.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MockUpQA : MonoBehaviour
+{
+    string[] quest1 = { "What day does the New Year begin?", "Everyday", "January 1st", "June 1st", "This is ridiculous"};
+    string quest1Ans = "January 1st";
+    string[] quest2 = { "What color are sith lightsabers?", "Red", "Blue", "Purple", "Green"};
+    string quest2Ans = "Red";
+    string[] quest3 = { "Complete the quote: \"With great power, comes great _______\"", "Responsibility", "Control", "Disgust", "Happiness" };
+    string quest3Ans = "Responsibility";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
