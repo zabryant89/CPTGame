2bb8fa1fa00dfde2b565feeab29f5606be0c0f1c
zabryant89 <zachary.a.bryant1@gmail.com>
2023-01-29 16:40:34 -0500

    Backup Old Code
    Backed up old code.  Beginning some early work on cleaning up code to prep for some of the other things needed this week (specifically the questions/answers being filled, randomized, etc).

---
+++ OldAssets/MockUpQA.cs
@@ -0,0 +1,118 @@
+/*Author: Zachary Bryant
+ * NOTES: This is intentionally completed as a non-generalized program.  There are options here to give students
+ *      that may be given to help them learn how to generalize solutions so it can accept the three questions assigned
+ *      OR more!
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MockUpQA : MonoBehaviour
+{
+    private string[] quest1 = { "What day does the New Year begin?", "Everyday", "January 1st", "June 1st", "This is ridiculous"};
+    private string quest1Ans = "January 1st";
+    private string[] quest2 = { "What color are sith lightsabers?", "Red", "Blue", "Purple", "Green"};
+    private string quest2Ans = "Red";
+    private string[] quest3 = { "Complete the quote: \"With great power, comes great _______\"", "Responsibility", "Control", "Disgust", "Happiness" };
+    private string quest3Ans = "Responsibility";
+
+    //getters:
+    //note: hardcoded but we can change that later.
+    public string GetQuestion1()
+    {
+        return quest1[0];
+    }
+
+    public string GetQuestion2()
+    {
+        return quest2[0];
+    }
+
+    public string GetQuestion3()
+    {
+        return quest3[0];
+    }
+
+    public string[] GetAnswers1()
+    {
+        string[] tmp = new string[4];
+        for (int i = 1; i < 5; i++)
+        {
+            tmp[i - 1] = quest1[i];
+        }
+        return tmp;
+    }
+
+    public string[] GetAnswers2()
+    {
+        string[] tmp = new string[4];
+        for (int i = 1; i < 5; i++)
+        {
+            tmp[i - 1] = quest2[i];
+        }
+        return tmp;
+    }
+
+    public string[] GetAnswers3()
+    {
+        string[] tmp = new string[4];
+        for (int i = 1; i < 5; i++)
+        {
+            tmp[i - 1] = quest3[i];
+        }
+        return tmp;
+    }
+
+    public string GetCorrect1()
+    {
+        return quest1Ans;
+    }
+
+    public string GetCorrect2()
+    {
+        return quest2Ans;
+    }
+
+    public string GetCorrect3()
+    {
+        return quest3Ans;
+    }
+
+    //generalized solution (may not work)
+    public string[] GenAnswers(string[] questSet)
+    {
+        string[] tmp = new string[4]; //assign a tmp set to store all answers
+        //insert the answers into tmp
+        for (int i = 0; i < 4; i++)
+        {
+            tmp[i] = questSet[i + 1]; //important: answers are from index 1 - 4
+        }
+
+        RandomizeSet(tmp, 0); //method to randomize the answers, start at index 0
+        return tmp;
+    }
+
+    public string[] RandomizeSet(string[] rand, int iteration)
+    {
+        if (iteration < 4)
+        {
+            //iteration: when = 4, stop
+            int swap = Random.Range(0, 3);
+
+            string tmp; //hold value if we need to do a swap
+            //if the swap # and the iteration # differ: then we swap those two values in the array!
+            if (iteration != swap)
+            {
+                tmp = rand[swap];
+                rand[swap] = rand[iteration];
+                rand[iteration] = tmp;
+            }
+            return RandomizeSet(rand, iteration + 1);
+        }
+        else
+        {
+            return rand;
+        }
+    }
+}
